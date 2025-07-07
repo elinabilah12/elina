dd0d7990c704a3380741352acabf7d8c7d19961f
elinabilah12 <21083010100@student.upnjatim.ac.id>
2025-07-07 13:55:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
-
-import streamlit as st
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
